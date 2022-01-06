f02f720d1cc4712d69ad8f8c350aff072615c311
Digaax <diogopequito@outlook.pt>
2022-01-06 18:59:29 +0000

    FileController

--- validator-main/src/main/java/tml/centralapi/validatormain/controller/FileController.java
+++ validator-main/src/main/java/tml/centralapi/validatormain/controller/FileController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import tml.centralapi.validatormain.model.*;
-import tml.centralapi.validatormain.repository.UploadHistoricRepository;
+import tml.centralapi.validatormain.repository.*;
 import tml.centralapi.validatormain.services.FileStorageService;
 
 import java.io.File;
@@ -45,6 +45,27 @@ import java.util.UUID;
 @RestController
 @CrossOrigin("*")
 public class FileController {
+
+    @Autowired
+    AgencyRepository agencyRepository;
+    @Autowired
+    CalendarDateRepository calendarDateRepository;
+    @Autowired
+    FeedInfoRepository feedInfoRepository;
+    @Autowired
+    FrequencyRepository frequencyRepository;
+    @Autowired
+    RouteRepository routeRepository;
+    @Autowired
+    ShapeRepository shapeRepository;
+    @Autowired
+    StopRepository stopRepository;
+    @Autowired
+    StopTimeRepository stopTimeRepository;
+    @Autowired
+    TripRepository tripRepository;
+    @Autowired
+    CalendarRepository calendarRepository;
 
     @Autowired
     FileStorageService storageService;
@@ -95,9 +116,11 @@ public class FileController {
                         System.out.println("Key: " + k + ": Value: " + v);
 
                         switch (k){
+
                             /*case "stops.txt":
                                 List<GtfsStop> stops = (List<GtfsStop>) v.getEntities();
                                 break;*/
+
                             case "agency.txt":
                                 List<GtfsAgency> agencies = (List<GtfsAgency>) v.getEntities();
 
@@ -105,13 +128,83 @@ public class FileController {
                                     GtfsAgencyIntendedOffer a = new GtfsAgencyIntendedOffer();
 
                                     a.setAgencyId(agency.agencyId());
-                                });
 
+                                    agencyRepository.save(a);
+                                });
+                                break;
+
+                            case "calendar_dates.txt":
+                                List<GtfsCalendarDate> calendarDates = (List<GtfsCalendarDate>) v.getEntities();
+
+                                calendarDates.forEach( (calendarDate) ->{
+                                    GtfsCalendarDateIntendedOffer cd = new GtfsCalendarDateIntendedOffer();
+
+                                    cd.setServiceId(calendarDate.serviceId());
+                                    cd.setCalendarName(calendarDate.CalendarName());
+                                    cd.setHoliday(calendarDate.holiday());
+                                    cd.setPeriod(calendarDate.period());
+
+                                    calendarDateRepository.save(cd);
+                                });
+                                break;
+
+                            case "stop_times.txt":
+                                List<GtfsStopTime> stopTimes = (List<GtfsStopTime>) v.getEntities();
+
+                                stopTimes.forEach( (stopTime) ->{
+                                    GtfsStopTimeIntendedOffer st = new GtfsStopTimeIntendedOffer();
+
+                                    st.setTripId(stopTime.tripId());
+                                    st.setStopId(stopTime.stopId());
+                                    st.setStopSequence(stopTime.stopSequence());
+                                    st.setStopHeadsign(stopTime.stopHeadsign());
+                                    st.setContinuousPickup(stopTime.continuousPickup());
+                                    st.setContinuousDropOff(stopTime.continuousDropOff());
+                                    st.setShapeDistTraveled(stopTime.shapeDistTraveled());
+                                    st.setTimepoint(stopTime.timepoint());
+
+                                    stopTimeRepository.save(st);
+                                });
+                                break;
+
+                            case "calendar.txt":
+                                List<GtfsCalendar> calendars = (List<GtfsCalendar>) v.getEntities();
+
+                                calendars.forEach( (calendar) ->{
+                                    GtfsCalendarIntendedOffer c = new GtfsCalendarIntendedOffer();
+
+                                    c.setServiceId(calendar.serviceId());
+                                    c.setCalendarName(calendar.calendarName());
+                                    c.setPeriod(calendar.period());
+                                    c.setMonday(calendar.monday());
+                                    c.setTuesday(calendar.tuesday());
+                                    c.setWednesday(calendar.wednesday());
+                                    c.setThursday(calendar.thursday());
+                                    c.setFriday(calendar.friday());
+                                    c.setSaturday(calendar.saturday());
+                                    c.setSunday(calendar.sunday());
+
+                                    calendarRepository.save(c);
+                                });
+                                break;
+
+                            case "shpes.txt":
+                                List<GtfsShape> shapes = (List<GtfsShape>) v.getEntities();
+
+                                shapes.forEach( (shape) ->{
+                                    GtfsShapeIntendedOffer sh = new GtfsShapeIntendedOffer();
+
+                                    sh.setShapeId(shape.shapeId());
+                                    sh.setShapePtLat(shape.shapePtLat());
+                                    sh.setShapePtLon(shape.shapePtLon());
+                                    sh.setShapePtSequence(shape.shapePtSequence());
+                                    sh.setShapeDistTraveled(shape.shapeDistTraveled());
+
+                                    shapeRepository.save(sh);
+                                });
                                 break;
                         }
-
                     });
-
 
                 } catch (Exception e) {
                     System.out.println(e);

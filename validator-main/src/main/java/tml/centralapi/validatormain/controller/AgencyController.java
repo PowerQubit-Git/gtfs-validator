b38c4b2d21e33c9a55ae3618ae0df4f15a77c827
danudnas <danudnas@hotmail.com>
2022-01-17 14:55:58 +0000

    Agency.txt Controller

---
+++ validator-main/src/main/java/tml/centralapi/validatormain/controller/AgencyController.java
@@ -0,0 +1,69 @@
+package tml.centralapi.validatormain.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tml.centralapi.validatormain.model.GtfsAgencyIntendedOffer;
+import tml.centralapi.validatormain.model.GtfsStopIntendedOffer;
+import tml.centralapi.validatormain.repository.AgencyRepository;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/pg-api/")
+@CrossOrigin("*")
+public class AgencyController {
+
+    @Autowired
+    AgencyRepository agencyRepository;
+
+    @GetMapping("/agency/{id}")
+    HttpEntity<List<GtfsAgencyIntendedOffer>> get(@PathVariable String id) throws Exception {
+        try {
+            List<GtfsAgencyIntendedOffer> list = (List<GtfsAgencyIntendedOffer>) agencyRepository.findByAgencyId(id);
+            return new ResponseEntity<>(list, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/agency")
+    public GtfsAgencyIntendedOffer create(@Valid @RequestBody GtfsAgencyIntendedOffer agency) {
+        return agencyRepository.save(agency);
+    }
+
+    @PutMapping("/agency/{id}")
+    public ResponseEntity<GtfsAgencyIntendedOffer> update(@PathVariable(value = "id") String id,
+                                                        @Valid @RequestBody GtfsAgencyIntendedOffer details) throws Exception {
+        try {
+            GtfsAgencyIntendedOffer agency = agencyRepository.findByAgencyId(id);
+            agency.setAgencyId(details.getAgencyId());
+            agency.setAgencyName(details.getAgencyName());
+            agency.setAgencyUrl(details.getAgencyUrl());
+            agency.setAgencyTimezone(details.getAgencyTimezone());
+            agency.setAgencyLang(details.getAgencyLang());
+            final GtfsAgencyIntendedOffer updatedStop = agencyRepository.save(agency);
+            return ResponseEntity.ok(updatedStop);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/agency/{id}")
+    public Map<String, Boolean> deleteAgency(@PathVariable(value = "id") Long id)
+            throws Exception {
+        GtfsAgencyIntendedOffer employee = agencyRepository.findById(id)
+                .orElseThrow(() -> new Exception("not found"));
+
+        agencyRepository.delete(employee);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+
+}
--- validator-main/src/main/java/tml/centralapi/validatormain/repository/AgencyRepository.java
+++ validator-main/src/main/java/tml/centralapi/validatormain/repository/AgencyRepository.java
@@ -2,6 +2,11 @@ package tml.centralapi.validatormain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import tml.centralapi.validatormain.model.GtfsAgencyIntendedOffer;
+import tml.centralapi.validatormain.model.GtfsStopIntendedOffer;
+
+import java.util.List;
 
 public interface AgencyRepository extends JpaRepository<GtfsAgencyIntendedOffer, Long> {
+    List<GtfsAgencyIntendedOffer> findByFeedId(String id);
+    GtfsAgencyIntendedOffer findByAgencyId(String id);
 }

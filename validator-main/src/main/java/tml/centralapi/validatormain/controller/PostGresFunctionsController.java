2f045df7356e81fb71021d397b75f763660eb943
danudnas <danudnas@hotmail.com>
2022-01-20 17:38:47 +0000

    Query Parameter sp_get_rows_by_table

--- validator-main/src/main/java/tml/centralapi/validatormain/controller/PostGresFunctionsController.java
+++ validator-main/src/main/java/tml/centralapi/validatormain/controller/PostGresFunctionsController.java
@@ -5,7 +5,10 @@ import tml.centralapi.validatormain.model.RowsByTable;
 import tml.centralapi.validatormain.model.SpTripsByLine;
 
 import javax.persistence.EntityManager;
+import javax.persistence.ParameterMode;
 import javax.persistence.PersistenceContext;
+import javax.persistence.StoredProcedureQuery;
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,14 +32,30 @@ public class PostGresFunctionsController {
     @GetMapping("count-rows-by-table/{feedId}")
     public List<RowsByTable> countRowsTable(@PathVariable String feedId) {
         try {
-            List<RowsByTable> list = entityManager
-                    .createStoredProcedureQuery("sp_get_rows_by_table", RowsByTable.class).getResultList();
+            StoredProcedureQuery query = entityManager.createStoredProcedureQuery( "sp_get_rows_by_table" );
+            List<RowsByTable> list = query.registerStoredProcedureParameter( feedId,  RowsByTable.class, ParameterMode.IN ).getResultList();
+            query.setParameter( 1, 1L );
+            query.execute();
             return list;
-        } catch (Exception e) {
-            System.out.println(e);
         }
-        return new ArrayList<>();
+        finally {
+            entityManager.close();
+            return new ArrayList<>();
+        }
+
     }
+
+//    @GetMapping("count-rows-by-table/{feedId}")
+//    public List<RowsByTable> countRowsTable(@PathVariable String feedId) {
+//        try {
+//            List<RowsByTable> list = entityManager
+//                    .createStoredProcedureQuery("sp_get_rows_by_table", RowsByTable.class).getResultList();
+//            return list;
+//        } catch (Exception e) {
+//            System.out.println(e);
+//        }
+//        return new ArrayList<>();
+//    }
 
 
 

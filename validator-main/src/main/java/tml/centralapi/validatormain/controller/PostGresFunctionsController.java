07faa5e403c4283a7ebb09689e5670bbc02b7096
Luis Neves <luis.neves1984@gmail.com>
2022-01-20 12:28:41 +0000

    postgres

--- validator-main/src/main/java/tml/centralapi/validatormain/controller/PostGresFunctionsController.java
+++ validator-main/src/main/java/tml/centralapi/validatormain/controller/PostGresFunctionsController.java
@@ -1,9 +1,8 @@
 package tml.centralapi.validatormain.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import tml.centralapi.validatormain.model.SpRowsByTable;
 import tml.centralapi.validatormain.model.SpTripsByLine;
 
 import javax.persistence.EntityManager;
@@ -19,7 +18,7 @@ public class PostGresFunctionsController {
     EntityManager entityManager;
 
     @GetMapping("trips-by-line")
-    List GetSpNumberOfTripsByLine() throws Exception {
+    List<SpTripsByLine> GetSpNumberOfTripsByLine() throws Exception {
         try {
             List<SpTripsByLine> list = entityManager.createStoredProcedureQuery("sp_number_of_trips_by_lines", SpTripsByLine.class).getResultList();
             return list;
@@ -29,19 +28,18 @@ public class PostGresFunctionsController {
         return new ArrayList<>();
     }
 
-//    CREATE OR REPLACE FUNCTION public.sp_number_of_trips_by_lines()
-//    RETURNS TABLE(total bigint, line_id varchar(50))
-//    LANGUAGE plpgsql
-//    AS $function$
-//    BEGIN
-//    RETURN QUERY select count(*), cast(rio.line_id as varchar(50)) from stop_times stio
-//    inner join trips tio on stio.trip_id = tio.trip_id
-//    inner join routes rio on rio.route_id = tio.route_id
-//    where stio.stop_sequence = 1
-//    group by rio.line_id;
-//    END;
-//    $function$
-//    ;
+    @GetMapping("count-rows-by-table")
+    public List<SpRowsByTable> countRowsTable() {
+        try {
+            List<SpRowsByTable> list = entityManager
+                    .createStoredProcedureQuery("sp_get_rows_by_table", SpRowsByTable.class).getResultList();
+            return list;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return new ArrayList<>();
+    }
+
 
 
 }
